600fda4d9364ac097b7b8dc99da897bcd27d5d71
milesChild <colechild1@gmail.com>
2024-11-30 16:54:22 -0500

    docker-compose setup and subproject Dockerfiles. Tested.

---
+++ dashboard/dashboard.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+# Set page config
+st.set_page_config(
+    page_title="labfolio",
+    page_icon="🧪",
+    layout="wide"
+)
+
+# Title and description
+st.title("labfolio")
+st.markdown("<labfolio description>")
